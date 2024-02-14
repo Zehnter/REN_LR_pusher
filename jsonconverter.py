99d9b27249e1169de7649b90d59f495596fd9fa4
Zehnter <129220104+Zehnter@users.noreply.github.com>
2024-02-14 05:46:27 +0100

    txt to json converter

---
+++ jsonconverter.py
@@ -0,0 +1,16 @@
+# incomplete code.
+
+sourcefile = 'resultssource.txt'
+
+result_dict = {}
+
+dict_structure = {
+    "Player": (),
+    "Kills": (),
+    "Deaths": (),
+    "K/D": (),
+    "Credits": (),
+    "Score": (),
+    "Team": (),
+
+}
